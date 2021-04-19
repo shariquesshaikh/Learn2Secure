b39838c969186b60db0367488d20a7b3eaa6ac4b
sk-sharique <sharique.shaikh.0123@gmail.com>
2021-04-19 23:52:33 +0530

    architecture decided

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/intermediate')
+def intermediate():
+    return render_template('intermediate.html')
+
+@app.route('/advanced')
+def advanced():
+    return render_template('advanced.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
