1f9d2eb70731297819927ae0ccac21d5a8f948e7
furqaan <thakurfurqaan@gmail.com>
2021-04-22 11:51:00 +0530

    Minor changes

--- app.py
+++ app.py
@@ -6,68 +6,81 @@ app = Flask(__name__)
 
 app.secret_key = 'n0T@talls1mpl3'
 
-client = MongoClient("mongodb+srv://l2s_user:ltos_yous3R@l2c.xzbms.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = MongoClient(
+    "mongodb+srv://l2s_user:ltos_yous3R@l2c.xzbms.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = client.get_database('l2c_db')
 data = db.reg_users
+
 
 # Default URL
 @app.route('/')
 def home():
-    return render_template('index.html')
+    name = ''
+    if session.get('logged_in'):
+        name = session['name']
+    return render_template('index.html', name = name)
 
-#Registration Request
+# Registration Request
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST' and 'name' in request.form and 'email' in request.form and 'password' in request.form and 'cpassword' in request.form:
-        exist = data.find_one({'email' : request.form['email']})
+        exist = data.find_one({'email': request.form['email']})
         if (exist is None):
             if (request.form['password'] == request.form['cpassword']):
                 hashedpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
-                data.insert({'_id' : uuid.uuid4().hex,'name' : request.form['name'],'email' : request.form['email'], 'password' : hashedpass})
+                data.insert({'_id': uuid.uuid4().hex, 'name': request.form['name'], 'email': request.form['email'],
+                             'password': hashedpass})
                 return redirect(url_for('login'))
     return render_template('register.html')
 
-#Login Request
+
+# Login Request
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
-        exist_user = data.find_one({'email' : request.form['email']})
+        exist_user = data.find_one({'email': request.form['email']})
         if exist_user:
-            if bcrypt.hashpw(request.form['password'].encode('utf-8'), exist_user['password']) == exist_user['password']:
+            if bcrypt.hashpw(request.form['password'].encode('utf-8'), exist_user['password']) == exist_user[
+                'password']:
                 session['name'] = exist_user['name']
                 session['email'] = request.form['email']
                 session['logged_in'] = True
                 return redirect(url_for('home'))
     return render_template('login.html')
 
-#Basic Content Page
+
+# Basic Content Page
 @app.route('/basics', methods=['GET', 'POST'])
 def basics():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
     return render_template('basics.html')
 
-#Intermediate Content Page
+
+# Intermediate Content Page
 @app.route('/intermediate', methods=['GET', 'POST'])
 def intermediate():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
     return render_template('intermediate.html')
 
-#Advanced Content Page
+
+# Advanced Content Page
 @app.route('/advanced', methods=['GET', 'POST'])
 def advanced():
     if not session.get('logged_in'):
-        return redirect(url_for('login'))    
+        return redirect(url_for('login'))
     return render_template('advanced.html')
 
-#Logout Request
+
+# Logout Request
 @app.route('/logout')
 def logout():
     session.pop('name', None)
     session.pop('email', None)
     session.pop('logged_in', None)
     return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8001, debug=True)
